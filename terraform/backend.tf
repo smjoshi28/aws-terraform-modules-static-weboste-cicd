f68bf361c171e48496c5d720bbd347ddbdb38d64
smjoshi28 <smjoshi@algomau.ca>
2025-10-31 13:14:37 -0400

    migrated terraform local backend to remote s3 backend

---
+++ terraform/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket              = "terraform-state-bucket-sj28"
+    key                 = "website/terraform.tfstate"
+    region              = "ca-central-1"
+    dynamodb_table      = "terraform_state_locks"
+  }
+}
