42caffe40ac94b9fa10a04a54a24cc54d36916f4
smjoshi28 <smjoshi@algomau.ca>
2025-11-02 01:01:38 -0400

    cache is left

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_providers { 
+  required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
@@ -13,26 +13,26 @@ provider "aws" {
 }
 
 module "backend" {
-    source                  = "./modules/remote_backend"
-    iam_user_name           = var.iam_user_name
-    bucket_name             = var.bucket_name
-    table_name              = var.table_name
+  source        = "./modules/remote_backend"
+  iam_user_name = var.iam_user_name
+  bucket_name   = var.bucket_name
+  table_name    = var.table_name
 }
 
 
 # module "dns_acm" {
-  #source                 = "./modules/route53_acm"
-  #root_domain            = var.root_domain
-  #dns_record_ttl         = var.dns_record_ttl
+#source                 = "./modules/route53_acm"
+#root_domain            = var.root_domain
+#dns_record_ttl         = var.dns_record_ttl
 #}
 
 module "s3_website" {
-  source                = "./modules/s3_website"
-  website_bucket        = var.website_bucket
-  force_destroy         = var.force_destroy
-  versioning_enabled    = var.versioning_enabled
-  index_document        = var.index_document
-  region                = var.region
+  source             = "./modules/s3_website"
+  website_bucket     = var.website_bucket
+  force_destroy      = var.force_destroy
+  versioning_enabled = var.versioning_enabled
+  index_document     = var.index_document
+  region             = var.region
 }
 
 /*module "cloudfront" {
